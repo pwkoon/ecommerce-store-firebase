8d6cb60b56def988f82e323e45b3fe500ac08ab4
wankoon <puah.wankoon@asiaone.com>
2025-02-24 15:07:41 +0800

    update env for firebase admin

--- firebase/firebase-admin.ts
+++ firebase/firebase-admin.ts
@@ -1,12 +1,16 @@
 import admin from "firebase-admin";
-import serviceAccount from "../serviceAccountKey.json";
+// import serviceAccount from "../serviceAccountKey.json";
 
 // Define the type of the service account
-const serviceAccountTyped = serviceAccount as admin.ServiceAccount;
+// const serviceAccountTyped = serviceAccount as admin.ServiceAccount;
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountTyped),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"), // Ensure newlines are handled properly
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
   });
 }
 
