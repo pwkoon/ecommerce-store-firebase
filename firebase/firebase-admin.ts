885e021f29a883dcbb949bc543a5149ecc07fca6
wankoon <puah.wankoon@asiaone.com>
2025-02-24 15:33:26 +0800

    remove service acct

--- firebase/firebase-admin.ts
+++ firebase/firebase-admin.ts
@@ -1,8 +1,4 @@
 import admin from "firebase-admin";
-// import serviceAccount from "../serviceAccountKey.json";
-
-// Define the type of the service account
-// const serviceAccountTyped = serviceAccount as admin.ServiceAccount;
 
 if (!admin.apps.length) {
   admin.initializeApp({
