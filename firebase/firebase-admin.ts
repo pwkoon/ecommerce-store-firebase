3d1de7c9ec5948126b5ac9fe01686a53fbe9004c
wankoon <puah.wankoon@asiaone.com>
2025-02-24 15:43:53 +0800

    update yaa

--- firebase/firebase-admin.ts
+++ firebase/firebase-admin.ts
@@ -3,7 +3,7 @@ import admin from "firebase-admin";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"), // Ensure newlines are handled properly
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
